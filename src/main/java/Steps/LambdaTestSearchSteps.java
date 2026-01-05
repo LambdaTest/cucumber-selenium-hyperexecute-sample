4b9c1991d5cea3c7a1ce8771bf24a1d8b3ed76b5
Rahul Mishra <rahulmishra@lambdatest.com>
2026-01-05 21:22:25 +0530

    Upgraded dependency and fixed tests

--- src/main/java/Steps/LambdaTestSearchSteps.java
+++ src/main/java/Steps/LambdaTestSearchSteps.java
@@ -1,33 +1,30 @@
 package Steps;
 
+import Runner.TestRunner;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import static org.testng.Assert.assertEquals;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import Runner.TestRunner;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import static org.testng.Assert.assertEquals;
 
-public class LambdaTestSearchSteps extends TestRunner
-{
+
+public class LambdaTestSearchSteps extends TestRunner {
 
     public RemoteWebDriver driver = this.connection;
     String test_url = "https://www.duckduckgo.com/";
     String expected_title = "LambdaTest Blogs";
 
     @Given("^that I am on the DuckDuckGo Search Page$")
-    public void user_on_duck_duck_go_page()
-    {
+    public void user_on_duck_duck_go_page() {
         System.out.println(driver.getCapabilities());
         driver.get(test_url);
     }
 
     @Then("^search for LambdaTest Blog$")
-    public void search_for_lambdatest_blog() throws InterruptedException
-    {
+    public void search_for_lambdatest_blog() throws InterruptedException {
         WebElement search_box = driver.findElement(By.cssSelector("#search_form_input_homepage"));
         search_box.click();
         search_box.sendKeys("LambdaTest Blog" + Keys.ENTER);
@@ -35,16 +32,14 @@ public class LambdaTestSearchSteps extends TestRunner
     }
 
     @Then("^click on the available result$")
-    public void click_n_available_result() throws InterruptedException
-    {
+    public void click_n_available_result() throws InterruptedException {
         WebElement search_result = driver.findElement(By.xpath("//div[@id='links']/div[1]//a[.='LambdaTest Blogs']"));
         search_result.click();
         Thread.sleep(2000);
     }
 
     @Then("^compare results$")
-    public void compare_result()
-    {
+    public void compare_result() {
         String page_title = driver.getTitle();
         assertEquals(page_title, expected_title);
     }
--- src/main/java/Steps/ToDoSteps.java
+++ src/main/java/Steps/ToDoSteps.java
@@ -1,50 +1,43 @@
 package Steps;
 
+import Runner.TestRunner;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 
-import Runner.TestRunner;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 
-public class ToDoSteps extends TestRunner
-{
-	public RemoteWebDriver driver = this.connection;
+public class ToDoSteps extends TestRunner {
+    public RemoteWebDriver driver = this.connection;
 
-	@Given("^user is on home Page$")
-	public void user_already_on_home_page()
-	{
-		System.out.println(driver.getCapabilities());
-		driver.get("https://lambdatest.github.io/sample-todo-app/");
+    @Given("^user is on home Page$")
+    public void user_already_on_home_page() {
+        System.out.println(driver.getCapabilities());
+        driver.get("https://lambdatest.github.io/sample-todo-app/");
+    }
 
-	}
+    @When("^select First Item$")
+    public void select_first_item() {
+        driver.findElement(By.name("li1")).click();
+    }
 
-	@When("^select First Item$")
-	public void select_first_item()
-	{
-		driver.findElement(By.name("li1")).click();
-	}
+    @Then("^select second item$")
+    public void select_second_item() {
+        driver.findElement(By.name("li2")).click();
+    }
 
-	@Then("^select second item$")
-	public void select_second_item()
-	{
-		driver.findElement(By.name("li2")).click();
-	}
+    @Then("^add new item$")
+    public void add_new_item() {
+        driver.findElement(By.id("sampletodotext")).clear();
+        driver.findElement(By.id("sampletodotext")).sendKeys("Yey, Let's add it to list");
+        driver.findElement(By.id("addbutton")).click();
+    }
 
-	@Then("^add new item$")
-	public void add_new_item()
-	{
-		driver.findElement(By.id("sampletodotext")).clear();
-		driver.findElement(By.id("sampletodotext")).sendKeys("Yey, Let's add it to list");
-		driver.findElement(By.id("addbutton")).click();
-	}
-
-	@Then("^verify added item$")
-	public void verify_added_item()
-	{
-		String item = driver.findElement(By.xpath("/html/body/div/div/div/ul/li[6]/span")).getText();
-		Assert.assertTrue(item.contains("Yey, Let's add it to list"));
-	}
+    @Then("^verify added item$")
+    public void verify_added_item() {
+        String item = driver.findElement(By.xpath("//input[@name='li6']/following-sibling::span")).getText();
+        Assert.assertTrue(item.contains("Yey, Let's add it to list"));
+    }
 }
